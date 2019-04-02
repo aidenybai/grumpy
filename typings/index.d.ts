f2b0f5b6eecabc15805d26c85c29d7aa63bfec86
Aiden Bai <38025074+cringiest@users.noreply.github.com>
2019-04-01 22:49:24 -0700

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -5,7 +5,7 @@ declare class Grumpy<K, V> extends Map<K, V> {
 	public array(): V[];
 	public clone(): Grumpy<K, V>;
 	public concat(...groups: Grumpy<K, V>[]): Grumpy<K, V>;
-  public cut(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean): [Grumpy<K, V>, Grumpy<K, V>];
+  	public cut(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean): [Grumpy<K, V>, Grumpy<K, V>];
 	public each(fn: (value: V, key: K, group: Grumpy<K, V>) => void, thisArg?: any): Grumpy<K, V>;
 	public equals(group: Grumpy<any, any>): boolean;
 	public every(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean, thisArg?: any): boolean;
@@ -26,7 +26,7 @@ declare class Grumpy<K, V> extends Map<K, V> {
 	public randomKey(): K | undefined;
 	public randomKey(count: number): K[];
 	public reduce<T>(fn: (accumulator: T, value: V, key: K, group: Grumpy<K, V>) => T, initialValue?: T): T;
-  public sift(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean, thisArg?: any): number;
+ 	public sift(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean, thisArg?: any): number;
 	public some(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean, thisArg?: any): boolean;
 	public sort(compareFunction?: (a: V, b: V, c?: K, d?: K) => number): Grumpy<K, V>;
 }
