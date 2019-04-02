c79a57e09d16e208baeb7f77343761011dc681e2
Aiden Bai <38025074+cringiest@users.noreply.github.com>
2019-04-01 22:49:09 -0700

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,12 +4,12 @@ declare class Grumpy<K, V> extends Map<K, V> {
 
 	public array(): V[];
 	public clone(): Grumpy<K, V>;
-	public concat(...collections: Grumpy<K, V>[]): Grumpy<K, V>;
-  public cut(fn: (value: V, key: K, collection: Grumpy<K, V>) => boolean): [Grumpy<K, V>, Grumpy<K, V>];
-	public each(fn: (value: V, key: K, collection: Grumpy<K, V>) => void, thisArg?: any): Grumpy<K, V>;
-	public equals(collection: Grumpy<any, any>): boolean;
-	public every(fn: (value: V, key: K, collection: Grumpy<K, V>) => boolean, thisArg?: any): boolean;
-	public filter(fn: (value: V, key: K, collection: Grumpy<K, V>) => boolean, thisArg?: any): Grumpy<K, V>;
+	public concat(...groups: Grumpy<K, V>[]): Grumpy<K, V>;
+  public cut(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean): [Grumpy<K, V>, Grumpy<K, V>];
+	public each(fn: (value: V, key: K, group: Grumpy<K, V>) => void, thisArg?: any): Grumpy<K, V>;
+	public equals(group: Grumpy<any, any>): boolean;
+	public every(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean, thisArg?: any): boolean;
+	public filter(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean, thisArg?: any): Grumpy<K, V>;
 	public first(): V | undefined;
 	public first(count: number): V[];
 	public firstKey(): K | undefined;
@@ -19,14 +19,15 @@ declare class Grumpy<K, V> extends Map<K, V> {
 	public last(count: number): V[];
 	public lastKey(): K | undefined;
 	public lastKey(count: number): K[];
-	public map<T>(fn: (value: V, key: K, collection: Grumpy<K, V>) => T, thisArg?: any): T[];
+	public map<T>(fn: (value: V, key: K, group: Grumpy<K, V>) => T, thisArg?: any): T[];
+	public mapValues<T>(fn: (value: V, key: K, group: Grumpy<K, V>) => T, thisArg?: any): Grumpy<K, T>;
 	public random(): V | undefined;
 	public random(count: number): V[];
 	public randomKey(): K | undefined;
 	public randomKey(count: number): K[];
-	public reduce<T>(fn: (accumulator: T, value: V, key: K, collection: Grumpy<K, V>) => T, initialValue?: T): T;
-  public sift(fn: (value: V, key: K, collection: Grumpy<K, V>) => boolean, thisArg?: any): number;
-	public some(fn: (value: V, key: K, collection: Grumpy<K, V>) => boolean, thisArg?: any): boolean;
+	public reduce<T>(fn: (accumulator: T, value: V, key: K, group: Grumpy<K, V>) => T, initialValue?: T): T;
+  public sift(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean, thisArg?: any): number;
+	public some(fn: (value: V, key: K, group: Grumpy<K, V>) => boolean, thisArg?: any): boolean;
 	public sort(compareFunction?: (a: V, b: V, c?: K, d?: K) => number): Grumpy<K, V>;
 }
 
